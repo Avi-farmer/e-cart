ca9154a7f331a16fef9647b0ecbbef48c544ab2c
sravanikella24062001 <sravanikella24062001@gmail.com>
2024-11-12 10:18:07 +0530

    new changes

--- src/Pages/Mobiles.jsx
+++ src/Pages/Mobiles.jsx
@@ -1,39 +1,81 @@
-import React from 'react'
-import { products } from '../data/ProductsData'
-import { Link } from 'react-router-dom'
-import Header from '../Components/Header'
-import { FaStar } from "react-icons/fa6";
+import React, { useState } from 'react';
+import { products } from '../data/ProductsData';
+import { Link } from 'react-router-dom';
+import Header from '../Components/Header';
+import { FaStar } from 'react-icons/fa6';
+import { Col, Row } from 'react-bootstrap';
 
 function Mobiles() {
-    const mobiles = products.slice(0, 20)
+
+    const [selectedBrands, setSelectedBrands] = useState([]);
+
+
+    const allBrands = [...new Set(products.map((product) => product.brand))];
+
+
+    const filteredMobiles = selectedBrands.length === 0
+        ? products.slice(0, 20)
+        : products.filter((product) => selectedBrands.includes(product.brand)).slice(0, 20);
+
+
+    const handleCheckboxChange = (brand) => {
+        setSelectedBrands((prevSelectedBrands) => {
+            if (prevSelectedBrands.includes(brand)) {
+
+                return prevSelectedBrands.filter((b) => b !== brand);
+            } else {
+
+                return [...prevSelectedBrands, brand];
+            }
+        });
+    };
+
     return (
         <>
             <Header />
-            <div className="body">
-                <div className='container'>
-                    <div className='data'>
-                        {
-                            mobiles.map(e => (
-                                <Link to={`/mobiles/${e.id}`} key={e.id} style={{ textDecoration: 'none' }}>
-                                    <div className='data1'>
-                                        <div className='dataimage'>
-                                            <img src={e.images} alt={e.brand} />
-                                        </div>
-                                        <div className='p-2 text-dark'>
-                                            <h6>{e.brand}</h6>
-                                            <h5>{e.price}/-</h5>
-                                            <p className='rating'>{e.rating}<FaStar /></p>
-                                            <small>{e.description.slice(0, 50)}</small>
-                                        </div>
-                                    </div>
-                                </Link>
-                            ))
-                        }
+            <Row className='container-fluid'>
+                <Col sm={2}>
+                    <div className="filters">
+                        <h5>Filter by Brand</h5>
+                        {allBrands.map((brand) => (
+                            <div key={brand} className="checkbox-group">
+                                <input
+                                    type="checkbox"
+                                    id={brand}
+                                    onChange={() => handleCheckboxChange(brand)}
+                                    checked={selectedBrands.includes(brand)}
+                                />
+                                <label className='ps-3'>{brand}</label>
+                            </div>
+                        ))}
                     </div>
-                </div>
-            </div>
+                </Col>
+
+                <Col>
+                    <div className="data">
+                        {filteredMobiles.map((e) => (
+                            <Link to={`/mobiles/${e.id}`} key={e.id} style={{ textDecoration: 'none' }}>
+                                <div className="data1">
+                                    <div className="dataimage">
+                                        <img src={e.images} alt={e.brand} />
+                                    </div>
+                                    <div className="p-2 text-dark">
+                                        <h6>{e.brand}</h6>
+                                        <h5>{e.price}/-</h5>
+                                        <p className="rating">
+                                            {e.rating}
+                                            <FaStar />
+                                        </p>
+                                        <small>{e.description.slice(0, 50)}</small>
+                                    </div>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </Col>
+            </Row>
         </>
-    )
+    );
 }
 
-export default Mobiles
+export default Mobiles;
